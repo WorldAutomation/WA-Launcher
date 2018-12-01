68bad41a04bbf9317118008759cb857d795da7e8
mindamp <mindamp@gmail.com>
2018-11-30 21:13:39 -0600

    Bleh... automation and stuff.

--- app/assets/js/configmanager.js
+++ app/assets/js/configmanager.js
@@ -9,7 +9,7 @@ const logger = require('./loggerutil')('%c[ConfigManager]', 'color: #a02d2a; fon
 const sysRoot = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME)
 const dataPath = path.join(sysRoot, '.worldautomation')
 fse.remove(path.join(sysRoot, '.worldautomation', 'instances', 'current', 'mods'))
-
+fse.mkdirp(path.join(sysRoot, '.worldautomation', 'instances', 'current', 'mods-optional'))
 const firstLaunch = !fs.existsSync(dataPath)
 
 exports.getAbsoluteMinRAM = function(){
